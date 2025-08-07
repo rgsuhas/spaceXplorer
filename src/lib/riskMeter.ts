d2fe4e8b99791f195aa8323e54185ae773701aa6
rgsuhas <rgsuhas6364@gmail.com>
2025-08-08 02:58:32 +0530

    remove other asteroid related files

--- src/lib/riskMeter.ts
+++
@@ -1,7 +0,0 @@
-export function getRiskStatus(isHazardous: boolean): { message: string; emoji: string } {
-  if (isHazardous) {
-    return { message: "Potentially Hazardous", emoji: "⚠️" };
-  } else {
-    return { message: "Not Potentially Hazardous", emoji: "✅" };
-  }
-}
